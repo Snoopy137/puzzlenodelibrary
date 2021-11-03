09ca7b60d9cff2c7e66e9778491f2d96956852b6
admin <admin@macbook-air.local>
2021-11-03 10:43:02 -0300

    v 0.3 (loan and return)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { startServer } from './sever';
 import { connect } from './config/typeorm';
+import { schedule } from 'node-cron';
+import {} from 'node
 
 async function main() {
+    schedule('* * * * *', () => {
+        console.log('running a task every minute');
+        main.addEventListener("click", modifyText, false);
+    });
     connect()
     const port: number = 4000;
     const app = await startServer();
